d14d7f16d2be734ee3230d10fb56c1057f61b64a
jacktuto <86354041+jacktuto@users.noreply.github.com>
2021-06-23 20:04:25 +0700

    Update watch.py

--- bot/modules/watch.py
+++ bot/modules/watch.py
@@ -17,10 +17,10 @@ def _watch(bot: Bot, update, isTar=False):
     try:
         link = message_args[1]
     except IndexError:
-        msg = f"/{BotCommands.WatchCommand} [youtube-dl supported link] [quality] |[CustomName] to mirror with youtube-dl.\n\n"
+        msg = f"/{BotCommands.Watch2Command} [youtube-dl supported link] [quality] |[CustomName] to mirror with youtube-dl.\n\n"
         msg += "<b>Note: Quality and custom name are optional</b>\n\nExample of quality: audio, 144, 240, 360, 480, 720, 1080, 2160."
         msg += "\n\nIf you want to use custom filename, enter it after |"
-        msg += f"\n\nExample:\n<code>/{BotCommands.WatchCommand} https://youtu.be/Pk_TthHfLeE 720 |Slam</code>\n\n"
+        msg += f"\n\nExample:\n<code>/{BotCommands.Watch2Command} https://youtu.be/Pk_TthHfLeE 720 |Slam</code>\n\n"
         msg += "This file will be downloaded in 720p quality and it's name will be <b>Slam</b>"
         sendMessage(msg, bot, update)
         return
@@ -62,9 +62,9 @@ def watch(update, context):
     _watch(context.bot, update)
 
 
-mirror_handler = CommandHandler(BotCommands.WatchCommand, watch,
+mirror_handler = CommandHandler(BotCommands.Watch2Command, watch,
                                 filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
-tar_mirror_handler = CommandHandler(BotCommands.TarWatchCommand, watchTar,
+tar_mirror_handler = CommandHandler(BotCommands.TarWatch2Command, watchTar,
                                     filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
 dispatcher.add_handler(mirror_handler)
 dispatcher.add_handler(tar_mirror_handler)
