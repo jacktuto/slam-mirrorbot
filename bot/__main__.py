977548f3ce24000f94d0f0a95fa564d3fcb31185
jacktuto <86354041+jacktuto@users.noreply.github.com>
2021-06-23 21:35:06 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -61,7 +61,7 @@ Type /{BotCommands.HelpCommand} to get a list of available commands
     uptime = get_readable_time((time.time() - botStartTime))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         if update.message.chat.type == "private" :
-            sendMessage(f"Hey I'm Alive 🙂\nSince: <code>{uptime}</code>", context.bot, update)
+            sendMessage(f"Halo Welcome, silahkan ketikkan /help apabila membutuhkan bantuan", context.bot, update)
         else :
             update.effective_message.reply_photo(IMAGE_URL, start_string, parse_mode=ParseMode.MARKDOWN, reply_markup=reply_markup)
     else :
